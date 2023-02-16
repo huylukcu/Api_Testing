71e0defcd241abe9de19b509cd3875a007873be6
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2023-02-16 12:24:19 -0600

    commit

---
+++ src/test/java/get_request/Get05.java
@@ -0,0 +1,41 @@
+package get_request;
+
+import base_urls.HerOkuAppBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
+import static org.testng.AssertJUnit.assertFalse;
+
+public class Get05 extends HerOkuAppBaseUrl {
+    /*
+     Given
+         https://restful-booker.herokuapp.com/booking
+     When
+         User sends get request to the URL
+     Then
+         Status code is 200
+       And
+           Among the data there should be someone whose firstname is "Adamz" and lastname is "Dear"
+  */
+    @Test
+    public void get05() {
+        //Set the url
+        spec.pathParam("first", "booking").
+                queryParams("firstname","Adamz","lastname","Dear");
+
+        //Set the expected data
+
+        //Send the request and get the response
+        Response response=given().spec(spec).when().get("/{first}");
+        response.prettyPrint();
+
+        //Do Assertion
+        //Status code is 200
+        assertEquals(200,response.statusCode());
+
+        //Among the data there should be someone whose firstname is "Adamz" and lastname is "Dear"
+        assertFalse(response.asString().contains("bookingid"));
+    }
+}
