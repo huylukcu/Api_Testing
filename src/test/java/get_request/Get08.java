f8dd98c2f9240c86c8682ce7fb93f7b69264355c
huylukcu <84544888+huylukcu@users.noreply.github.com>
2023-02-23 09:42:40 -0600

    Update Get08.java

--- src/test/java/get_request/Get08.java
+++ src/test/java/get_request/Get08.java
@@ -3,7 +3,6 @@ import base_urls.JsonPlaceHolderBaseUrl;
 import io.restassured.response.Response;
 import org.junit.Test;
 import test_data.JsonPlaceHolderTestData;
-
 import java.util.HashMap;
 import java.util.Map;
 
